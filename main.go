747eb486987bf5c42546e5ddea3de03b0723b38e
t8nax <t8naxx@gmail.com>
2025-04-09 18:10:55 +0300

    Remove API key

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/sirupsen/logrus"
@@ -13,19 +15,18 @@ func main() {
 	log := logrus.New()
 	log.SetLevel(logrus.TraceLevel)
 
-	// api_key, exists := os.LookupEnv("WEATHER_SERVICE_API_KEY")
+	api_key, exists := os.LookupEnv("WEATHER_SERVICE_API_KEY")
 
-	// if !exists {
-	// 	log.Fatal("Could not find weatheecr service API key in environment variables")
-	// }
+	if !exists {
+		log.Fatal("Could not find weatheecr service API key in environment variables")
+	}
 
 	app := fiber.New()
 	app.Use(recover.New())
 	app.Use(middleware.LogMiddleware(log))
 	app.Use(middleware.ErrorMiddleware(log))
 
-	// registry := registry.NewRegistry(api_key, log)
-	registry := registry.NewRegistry("L7MWV7NHX8UKDEH75UP8UNDM5", log)
+	registry := registry.NewRegistry(api_key, log)
 	app = router.NewRouter(app, registry.NewAppController())
 
 	app.Listen(":3000")
